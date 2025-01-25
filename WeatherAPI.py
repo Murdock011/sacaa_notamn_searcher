4a46ede2ebccd85a3ab9f607683cf4e8d5f6aea1
Murdock011 <gerhard.duminy@gmail.com>
2025-01-26 01:47:54 +0200

    add weather file
    metar and taf requests are working

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -4,26 +4,65 @@ import urllib.request
 from os import path
 import pprint
 import csv
+import datetime
 
 
 class weatherAPI():
 	def __init__(self):
-		if not path.isfile('metars.cache.csv.gz'):
+		if not path.isfile('metars.cache.csv') or not path.isfile('tafs.cache.csv'):
 			self.update_weather()
+		self.meatars, self.tafs = self.loadweather()
+
+	def getTaf(self,icao):
+		try:	
+			return self.tafs[icao.upper()]
+		except Exception as e:
+			return False
+
+	def getMetar(self,ico):
+		try:
+			return self.meatars[icao.upper()]
+		except Exception as e:
+			return False
+
+	def getUpdatedDate(self):
+		return self.meatars['DATE']
 
 	def loadweather(self):
-			
+		types = ["metars","tafs"]
+		result_list = []
+		for t in types:
+			result_dict = {}
+			if not path.isfile(f'{t}.csv'):
+				with open(f'{t}.cache.csv', mode='r', encoding='utf-8') as csv_file:
+					reader = csv.reader(csv_file)
+					for i , row in enumerate(reader):
+						if i >5:
+							value = row[0]
+							key = value[:4]
+							if key == "TAF ":
+								key = value[4:8]
+							if key == "AMD " or key == "COR":
+								key = value[8:12]
+							result_dict[key.strip()] = value.strip() 
+							with open(f'{t}.csv', 'w') as csv_file:
+								csv_file.write(f"DATE {datetime.datetime.now().strftime("%d%b%y %H%M %z")} \n")
+								for a in list(result_dict.values() ):
+									csv_file.write(a+"\n")
 
-
-		result_dict = {}
-		with open('metars.cache.csv', mode='r', newline='', encoding='utf-8') as csv_file:
-			reader = csv.reader(csv_file)
-			for i , row in enumerate(reader):
-				if i >5:
-					value = row[0]
-					key = value[:4]
-					result_dict[key.strip()] = value.strip() 
-		pprint.pp(result_dict["FALW"])
+			else:
+				with open(f'{t}.csv', mode='r', encoding='utf-8') as csv_file:
+					reader = csv.reader(csv_file)
+					for i , row in enumerate(reader):
+						value = row[0]
+						key = value[:4]
+						if key == "TAF ":
+							key = value[4:8]
+						if key == "AMD " or key == "COR":
+							key = value[8:12]
+						result_dict[key] = value
+			result_list.append(result_dict)	
+		return result_list[0],result_list[1]
 
 						
 
@@ -36,9 +75,15 @@ class weatherAPI():
 			with open('metars.cache.csv', 'wb') as f_out:
 				shutil.copyfileobj(f_in, f_out)
 
-
-
+#https://aviationweather.gov/data/cache/tafs.cache.csv.gz
+			urllib.request.urlretrieve(
+				"https://aviationweather.gov/data/cache/tafs.cache.csv.gz",
+				"tafs.cache.csv.gz",
+			)
+		with gzip.open('tafs.cache.csv.gz', 'rb') as f_in:
+			with open('tafs.cache.csv', 'wb') as f_out:
+				shutil.copyfileobj(f_in, f_out)
 
 if __name__ == "__main__":
 	weather = weatherAPI()
-	weather.loadweather()
+	print(weather.getTaf('faor'))
